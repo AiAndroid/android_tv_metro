d10054be90c6b2d8691892c158633a7e04dcc94e
liuhuadong78@gmail.com <liuhuadong7804@gmail.com>
2014-10-21 19:38:13 +0800

    add en support

--- android/Starfish/src/com/starfish/faq/FAQLoader.java
+++ android/Starfish/src/com/starfish/faq/FAQLoader.java
@@ -5,6 +5,8 @@ import android.content.res.Configuration;
 import com.tv.ui.metro.loader.TabsGsonLoader;
 import com.tv.ui.metro.model.DisplayItem;
 
+import java.util.Locale;
+
 /**
  * Created by liuhuadong on 10/20/14.
  */
@@ -15,8 +17,15 @@ public class FAQLoader extends TabsGsonLoader {
 
     @Override
     public void setLoaderURL(DisplayItem item) {
-        calledURL = "https://raw.githubusercontent.com/AiAndroid/stream/master/tv/game/home_faq.json";
-        if(getContext().getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT)
+        if(getContext().getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
             calledURL = "https://raw.githubusercontent.com/AiAndroid/stream/master/tv/game/home_faq_mobile.json";
+        }else {
+            Locale locale = getContext().getResources().getConfiguration().locale;
+            if (locale.getLanguage().equalsIgnoreCase("zh")) {
+                calledURL = "https://raw.githubusercontent.com/AiAndroid/stream/master/tv/game/home_faq.json";
+            } else {
+                calledURL = "https://raw.githubusercontent.com/AiAndroid/stream/master/tv/game/home_faq_en.json";
+            }
+        }
     }
 }
